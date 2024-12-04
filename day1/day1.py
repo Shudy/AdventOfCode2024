a53c62b84e49bc56b3a9696bb5270c621656ca02
Shudy <elias.ortizreyes@gmail.com>
2024-12-04 01:35:47 +0100

    Add new funtion for Day 1: Part 2

--- day1/day1.py
+++ day1/day1.py
@@ -23,12 +23,28 @@ def calculate_distance():
     return total_distance
 
 
-if __name__ == '__main__':
+def count_similarities():
+    total_similarity = 0
+    for i in range(len(first_numbers)):
+        number = first_numbers[i]
+        number_occurrences = second_numbers.count(number)
+        number_similarity = number * number_occurrences
+        total_similarity += number_similarity
+    return total_similarity
+
+
+def main():
     read_data('../data/day1')
+
+    # Part 1: Calculate the distance between the two lists
     sort_min_to_max(first_numbers)
     sort_min_to_max(second_numbers)
 
-    print(f'First list: {first_numbers}')
-    print(f'First list: {second_numbers}')
+    print(f'Total distance: {calculate_distance()}')
 
-    print(f"Total distance: {calculate_distance()}")
+    # Part 2: Calculate the similarity between the two lists
+    print(f'Similarity: {count_similarities()}')
+
+
+if __name__ == '__main__':
+    main()
