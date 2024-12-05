c0e52f889a6f62768682d6cb1fd8eb1546eaea13
Shudy <elias.ortizreyes@gmail.com>
2024-12-05 01:31:56 +0100

    Add new code for Day2: Part 2

--- day1/day2.py
+++ day1/day2.py
@@ -20,14 +20,40 @@ def is_report_safe(report: list[int]):
     return False
 
 
+def is_possible_to_fix_report(report: list[int]):
+    for i in range(len(report)):
+        alternative_report = report.copy()
+        alternative_report.pop(i)
+        if is_report_safe(alternative_report):
+            return True
+    return False
+
+
 def main():
     read_data('../data/day2')
-    number_of_save_reports = 0
+    unsafe_reports = []
+    # Part 1:
+    number_of_safe_reports = 0
     for report in reports:
         if is_report_safe(report):
-            number_of_save_reports += 1
+            number_of_safe_reports += 1
+        else:
+            unsafe_reports.append(report)
 
-    print(f'Number of save reports: {number_of_save_reports}')
+    print(f'--------PART 1--------')
+    print(f'Number of safe reports: {number_of_safe_reports}')
+    print(f'Number of unsafe reports: {len(unsafe_reports)}')
+
+    # Part 2:
+    fixed_reports = 0
+    for unsafe_report in unsafe_reports:
+        if is_possible_to_fix_report(unsafe_report):
+            fixed_reports += 1
+    total_safe_reports = number_of_safe_reports + fixed_reports
+
+    print(f'--------PART 2--------')
+    print(f'Number of fixed reports: {fixed_reports}')
+    print(f'Total safe reports: {total_safe_reports}')
 
 
 if __name__ == '__main__':
