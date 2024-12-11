605ef9fd640f8ff1b2b65d9b4ee93dbb1f9ca9f1
Shudy <elias.ortizreyes@gmail.com>
2024-12-11 13:52:28 +0100

    Day3: part 2, addded new advanced memory analisis

--- day3/day3.py
+++ day3/day3.py
@@ -3,7 +3,8 @@ import re
 memory_data = ''
 mul_operations = []
 
-mul_regex = r"mul\((\d{1,3}),(\d{1,3})\)"
+mul_regex = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+mul_regex_tokens = re.compile(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
 
 
 def read_data(path):
@@ -25,12 +26,33 @@ def execute_mul_operations():
     return result
 
 
+def execute_advanced_mul_operations():
+    mul_enabled = True
+    result = 0
+    tokens = re.findall(mul_regex_tokens, memory_data)
+    for token in tokens:
+        if token == "do()":
+            mul_enabled = True
+        elif token == "don't()":
+            mul_enabled = False
+        else:
+            if mul_enabled:
+                match = re.match(mul_regex, token)
+                if match:
+                    result += int(match.groups()[0]) * int(match.groups()[1])
+
+    return result
+
+
 def main():
     read_data('../data/day3')
 
     print(f'--------PART 1--------')
     find_mul_operations()
     print(f"Result of the memory: {execute_mul_operations()}")
+
+    print(f'--------PART 2--------')
+    print(f"Result of advances memory: {execute_advanced_mul_operations()}")
 
 
 if __name__ == '__main__':
